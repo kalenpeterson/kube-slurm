162dabcdbfa6cd0637ae092c4e88c55e5e6f7ca6
Kalen Peterson <kalen.peterson@siriuscom.com>
2021-04-20 08:27:17 -0700

    Add tests and wrappers

---
+++ tests/test-single-custom-image-job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Test a Wrapper without Slurm, Runs on the same node
+
+# Set Slurm ENV Vars
+export SLURM_GPUS=0
+export SLURM_JOB_ACCOUNT=unknown
+export SLURM_JOB_ID=99
+export SLURM_JOB_NAME=slurm-single-job
+export SLURM_NODEID=99
+export SLURMD_NODENAME=nvidia-node01
+export USER_HOME=${HOME}
+
+# Define the Image to run
+export KUBE_IMAGE=tensorflow:custom
+
+# Define the Script to run
+export KUBE_SCRIPT=/home/dgx/kube-slurm/containers/slurm-single-tf-job/test.sh
+
+# Call the Wrapper
+## DO NOT CHANGE ##
+../wrappers/kube-slurm-custom-image-job.sh
